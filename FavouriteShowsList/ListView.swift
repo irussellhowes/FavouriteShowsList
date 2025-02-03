0328a5021dce27eae20aa69a7157f5bcef2b1e5e
Isla Russell-Howes <irussellhowes@lcs.on.ca>
2025-02-03 11:30:09 -0500

    Initial Project Organization

--- FavouriteShowsList/ListView.swift
+++ FavouriteShowsList/ListView.swift
@@ -7,18 +7,15 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ListView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+
         }
         .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    ListView()
 }
