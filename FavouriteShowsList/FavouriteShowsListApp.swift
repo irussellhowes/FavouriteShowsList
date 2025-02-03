bceae63e42dd3052fc3a4bcebc710329b0155085
Isla Russell-Howes <irussellhowes@lcs.on.ca>
2025-02-03 11:28:44 -0500

    Initial Commit

---
+++ FavouriteShowsList/FavouriteShowsListApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavouriteShowsListApp.swift
+//  FavouriteShowsList
+//
+//  Created by Isla Russell-Howes on 2025-02-03.
+//
+
+import SwiftUI
+
+@main
+struct FavouriteShowsListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
